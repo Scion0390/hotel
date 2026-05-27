cb1cb7b595deef82c4822c06dfed9b76984c870c
Scion0390 <singhsciondeep@gmail.com>
2026-05-27 13:29:58 +0530

    last5

--- email.js
+++ email.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
     secure:false,
     auth:{
         user:process.env.BREVO_EMAIL,
-        pass:process.env.BREVO_SMPT_KEY,
+        pass:process.env.BREVO_SMPT_KEY
     }
 })
 
@@ -33,3 +33,40 @@ const sendEmail = async(email,otp)=>{
     }
 }
 module.exports = {generateOTP, sendEmail};
+
+
+/*const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+
+const generateOTP = ()=>{
+    return crypto.randomInt(100000,1000000).toString();
+}
+//SMTP = Simple Mail Transfer Protocol
+
+const transporter = nodemailer.createTransport({
+    host:"smtp-relay.brevo.com",
+    port:587,
+    secure:false,
+    auth:{
+        user:process.env.BREVO_EMAIL,
+        pass:process.env.BREVO_SMPT_KEY,
+    }
+})
+
+const sendEmail = async(email,otp)=>{
+    try{
+        const info = await transporter.sendMail({
+            from:`"mywebsite" <singhsciondeep@gmail.com>`,
+            to:email,
+            subject:"Your OTP code is",
+            html:`
+            <h1> your otp is ${otp} </h1>
+            `
+        })
+        return true;
+    }catch(err){
+        console.log(err);
+        return false
+    }
+}
+module.exports = {generateOTP, sendEmail};*/
